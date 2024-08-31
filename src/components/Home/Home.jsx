06256e07dc175d37545973d2d9ecfe16fc8863cb
zaheerkhan317 <gousezahir100@gmail.com>
2024-08-31 21:58:53 +0530

    Feat: Added Carousel Feature

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
-
+import img1 from '../../img/1.png'
+import img2 from '../../img/2.png'
+import img3 from '../../img/3.png'
+import { Carousel } from 'react-bootstrap'
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Carousel id="carouselExampleIndicators" interval={2000}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={img1} // Replace with your image source
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={img2} // Replace with your image source
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={img3} // Replace with your image source
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
   )
 }
 
